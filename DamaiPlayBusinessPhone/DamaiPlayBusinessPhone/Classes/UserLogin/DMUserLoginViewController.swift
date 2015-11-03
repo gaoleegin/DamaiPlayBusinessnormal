b23170d0068f4010fd24949e09084dd2bc70ab1a
lijungao <lijungao1999@163.com>
2015-11-03 23:47:33 +0800

    pod的添加

--- DamaiPlayBusinessPhone/DamaiPlayBusinessPhone/Classes/UserLogin/DMUserLoginViewController.swift
+++ DamaiPlayBusinessPhone/DamaiPlayBusinessPhone/Classes/UserLogin/DMUserLoginViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import SDWebImage
+import SVProgressHUD
 
 class DMUserLoginViewController: UIViewController {
-
+    
+    //版本
+    @IBOutlet weak var VersionLabel: UILabel!
+    
+    //用户名
+    @IBOutlet weak var UserName: UITextField!
+    
+    //密码
+    @IBOutlet weak var PassWord: UITextField!
+    
+    ///登录
+    @IBAction func userLogin() {
+        let userNameString = UserName.text;
+        let passwordString = PassWord.text;
+        
+        print("用户名\(userNameString)");
+        print("密码\(passwordString)");
+        
+        topConstraint.constant -= 20;
+    }
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let  label = UILabel()
-        
-
         // Do any additional setup after loading the view.
     }
 
@@ -23,16 +45,5 @@ class DMUserLoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
